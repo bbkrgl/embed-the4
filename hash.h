dea86318f6afd5b3547d6d96dc95019e2ca8f6d3
Burak Köroğlu <burak20012009@gmail.com>
2022-06-20 21:12:07 +0300

    Initial commit

---
+++ hash.h
@@ -0,0 +1,17 @@
+/* 
+ * File:   hash.h
+ * Author: e2448637
+ *
+ * Created on June 20, 2022, 9:07 PM
+ */
+
+#ifndef HASH_H
+#define	HASH_H
+
+#include <stdio.h>
+#include <string.h>
+
+void compute_hash(unsigned char *inp, unsigned char *out);
+
+#endif	/* HASH_H */
+
---
+++ main.c
@@ -0,0 +1,15 @@
+/*
+ * File:   main.c
+ * Author: e2448637
+ *
+ * Created on June 20, 2022, 9:06 PM
+ */
+
+
+#include "device.h"
+#include "hash.h"
+
+void main(void)
+{
+	return;
+}
